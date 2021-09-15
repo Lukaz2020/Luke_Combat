8f520d9c733077315bf1b3143b2042ab35d6e8b7
Lukaz2020 <62490330+Lukaz2020@users.noreply.github.com>
2021-09-15 15:59:55 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,5 +3,6 @@ games { 'gta5' }
 
 author 'Luke'
 description 'Realstic Combat system to remove crosshairs and combat rolling. As well as Healing and armor application'
+verison '1.0'
 
 client_script 'client.lua'
